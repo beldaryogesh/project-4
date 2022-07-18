b0d35983b7a961412e67e1ae90a64193108e1d2a
arjunvip100 <Arjunvip100@gmail.com>
2022-07-18 17:37:49 +0530

    final

--- src/controller/urlController.js
+++ src/controller/urlController.js
@@ -1,38 +1,57 @@
 const urlModel = require("../models/urlModel");
-const mongoose= require('mongoose');
-const config = require('config');
-const shortid= require('shortid');
+const mongoose = require('mongoose');
+const shortid = require('shortid');
 const { default: validator } = require("validator");
 const validUrl = require('valid-url');
+const validate = require("../validations/validate")
+const { urlRegex } = require("../validations/validate")
+
 const shorten = async function (req, res) {
-    try{
+    try {
         let host = "http:localhost:3000";
-    const data = req.body;
-    let longUrl=data.longUrl;
-    //checking base url
-    
-    if(!validUrl.isUri(longUrl)) {
-        return res
-        .status(401)
-        .send({ status: false, msg: "Invalid url" });
-    }
-    else {
-        const urlCode= shortid.generate();
-         const shortUrl= host + '/' + urlCode;
-         data.urlCode=urlCode;
-        data.shortUrl=shortUrl;
-        
-         
-            let datafull={urlCode: urlCode,
+        const data = req.body;
+        let longUrl = data.longUrl;
+        if (Object.keys(data).length == 0) {
+            return res.status(400).send({ status: false, msg: "Please provide data" })
+        };
+        if (Object.keys(longUrl).length == 0) {
+            return res.status(400).send({ status: false, msg: "Please provide longUrl" })
+        };
+        if(!urlRegex(longUrl)){
+            return res
+            .status(400)
+            .send({ status: false, message: "Url is not valid!!" })}
+
+        let isValidUrl= await urlModel.findOne({longUrl})
+        if(isValidUrl ){
+          return  res.status(400).send({status: false, message: "longUrl is alredy present"})
+        }
+        //checking base url
+
+        if (!validUrl.isUri(longUrl)) {
+            return res
+                .status(401)
+                .send({ status: false, msg: "Invalid url" });
+        }
+        else {
+            const urlCode = shortid.generate();
+            const shortUrl = host + '/' + urlCode;
+            data.urlCode = urlCode;
+            data.shortUrl = shortUrl;
+
+
+            let datafull = {
+                urlCode: urlCode,
                 shortUrl: shortUrl,
-                longUrl: longUrl};
-                
+                longUrl: longUrl
+            };
+
             const createdURL = await urlModel.create(datafull);
             return res
-            .status(200)
-        .send({ status: true, data: data });
+                .status(200)
+                .send({ status: true, data: data });
 
-    }
+        }
     }
     catch (err) {
         return res.status(500).send({ status: false, msg: err.message });
